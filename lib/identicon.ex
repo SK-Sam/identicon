dc898ad00a87876a07a43937417475244b79d80c
Sam Yeo <yeosamuel95@gmail.com>
2021-04-03 05:37:12 -0600

    Add pick_color function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -9,6 +9,7 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input()
+    |> pick_color
   end
 
   @doc """
@@ -26,5 +27,15 @@ defmodule Identicon do
     |> :binary.bin_to_list
 
     %Identicon.Image{hex: hex}
+  end
+
+  @doc """
+    Get RGB values based on byte value
+  """
+  def pick_color(image) do
+    %Identicon.Image{hex: hex_list} = image
+    [r, g, b | _tail] = hex_list
+
+    %Identicon.Image{image | color: {r, g, b}}
   end
 end
