64cde061075c66fd0176d804c8709bfa34b0c9cb
Sam Yeo <yeosamuel95@gmail.com>
2021-04-03 04:50:24 -0600

    Add function hash_input which passes test

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,16 +3,21 @@ defmodule Identicon do
   Documentation for `Identicon`.
   """
 
+  def main(input) do
+    input
+    |> hash_input()
+  end
+
   @doc """
-  Hello world.
+    Utilizes MD5 algorithm to hash a string and return
+    a List of 16 bytes.
 
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
-
+  ## Example
+      iex> Identicon.hash_input("sam")
+      [51, 37, 50, 220, 250, 161, 203, 246, 30, 42, 38, 107, 215, 35, 97, 44]
   """
-  def hello do
-    :world
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
   end
 end
